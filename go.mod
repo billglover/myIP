44917e66774c8d9f111beb0abeb881157350dd49
Bill Glover <bill@billglover.co.uk>
2018-10-17 23:37:32 +0100

    Add prometheus metrics

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/billglover/myIP
+
+require (
+	github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973 // indirect
+	github.com/gogo/protobuf v1.1.1 // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v0.9.0
+	github.com/prometheus/client_model v0.0.0-20180712105110-5c3871d89910 // indirect
+	github.com/prometheus/common v0.0.0-20181015124227-bcb74de08d37 // indirect
+	github.com/prometheus/procfs v0.0.0-20181005140218-185b4288413d // indirect
+	golang.org/x/net v0.0.0-20181017193950-04a2e542c03f // indirect
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
+)
--- main.go
+++ main.go
@@ -5,10 +5,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var ipsRequested = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "myip_requests_total",
+	Help: "The total number of IPs requested",
+})
 
 func main() {
 	http.HandleFunc("/ip", ipHandler)
+	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics/", promhttp.Handler())
+	
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -21,10 +33,13 @@ func main() {
 
 func ipHandler(res http.ResponseWriter, req *http.Request) {
 
+	ipsRequested.Inc()
+
 	ip := req.Header.Get("X-Forwarded-For")
 	if ip == "" {
 		log.Println("/ip, 404, X-Forwarded-For not set")
 		res.WriteHeader(http.StatusNotFound)
+		return
 	}
 	log.Printf("/ip, 200, %s\n", ip)
 	fmt.Fprintln(res, ip)
