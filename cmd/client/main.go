0cb0f3bc3be8a869d7efb85c7d7295ac48f798b0
Bill Glover <bill@billglover.co.uk>
2018-10-19 23:46:15 +0100

    Instrument failures and response times

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"strings"
+	"strconv"
 	"time"
 
 	"github.com/pkg/errors"
@@ -19,82 +19,91 @@ var (
 )
 
 var (
-	requestCount = prometheus.NewCounter(prometheus.CounterOpts{
+	reqCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "myip",
 		Subsystem: "client",
-		Name: "request_count",
-		Help: "the number of requests to the IP lookup URL",
+		Name:      "requests_total",
+		Help:      "IP request count",
+	}, []string{"code"})
+	reqErrCount = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "myip",
+		Subsystem: "client",
+		Name:      "requests_err_total",
+		Help:      "IP request error count",
 	})
 	changeCount = prometheus.NewCounter(prometheus.CounterOpts{
 		Namespace: "myip",
 		Subsystem: "client",
-		Name: "change_count",
-		Help: "the number of times the IP has changed",
+		Name:      "changes_total",
+		Help:      "the number of times the IP has changed",
 	})
+	reqDurations = prometheus.NewSummary(
+		prometheus.SummaryOpts{
+			Name:       "request_durations_seconds",
+			Help:       "IP request latency distributions",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		},
+	)
 )
 
 func main() {
-	prometheus.MustRegister(requestCount, changeCount)
-
-	ips := make(chan net.IP, 1)
-	updates := make(chan net.IP, 1)
-	go lookupIP(remote, 10 * time.Second, ips)
-	go watchForChange(ips, updates)
-
+	prometheus.MustRegister(reqCount, reqErrCount, reqDurations, changeCount)
 	http.Handle("/metrics/", promhttp.Handler())
+
+	watchForChange(remote, 10*time.Second)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func lookupIP(url string, d time.Duration, ips chan<- net.IP) {
-	t := time.NewTicker(d)
+func watchForChange(url string, d time.Duration) {
+	go func() {
+		prev := new(net.IP)
 
-	for range t.C {
-		ip, err := getIP(remote)
-		if err != nil {
-			log.Println(err)
-			continue
+		t := time.NewTicker(d)
+
+		for range t.C {
+			ip, err := getIP(remote)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			if ip.Equal(*prev) == false {
+				changeCount.Inc()
+				*prev = ip
+			}
 		}
-		ips <- ip
-	}
+	}()
 }
 
 func getIP(url string) (net.IP, error) {
-	// TODO:
-	// - must time out quickly
-	// - must keep metrics on failures
-	// - must keep metrics on response times
-	requestCount.Inc()
+	timer := prometheus.NewTimer(reqDurations)
+	defer timer.ObserveDuration()
 
 	req, err := http.NewRequest(http.MethodGet, remote, nil)
 	if err != nil {
+		reqErrCount.Inc()
 		return nil, errors.Wrap(err, "failed to create request")
 	}
 
-	client := http.DefaultClient
+	client := http.Client{Timeout: 1 * time.Second}
 	res, err := client.Do(req)
+
 	if res != nil {
+		reqCount.WithLabelValues(strconv.Itoa(res.StatusCode)).Inc()
 		defer res.Body.Close()
 	}
+
 	if err != nil {
+		reqErrCount.Inc()
 		return nil, errors.Wrap(err, "failed to make request")
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
+		reqErrCount.Inc()
 		return nil, errors.Wrap(err, "failed to parse response body")
 	}
 	ip := net.ParseIP(strings.TrimSpace(string(body)))
 	return ip, nil
-}
-
-func watchForChange(ips <-chan net.IP, updates chan <-net.IP) {
-	prev := new(net.IP)
-	for ip := range ips {
-		fmt.Println("current:", ip, "prev:", prev)
-		if ip.Equal(*prev) == false {
-			changeCount.Inc()
-			*prev = ip
-			updates<-ip
-		}
-	}
 }
